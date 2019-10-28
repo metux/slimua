b96f20a727701755624e362fbb4dc3cedf5b6c73
Enrico Weigelt, metux IT consult <info@metux.net>
2019-10-29 00:51:40 +0100

    1

--- slimua/gui/main_window.py
+++ slimua/gui/main_window.py
@@ -15,19 +15,24 @@ class MainWindow(Gtk.Window):
         self.mailstore = mailstore
 
         self.mailbox_tree_store = MailboxTreeStore(mailstore)
-        self.mailbox_tree_view = MailboxTreeView(self, treestore = self.mailbox_tree_store)
+        self.mailbox_tree_view  = MailboxTreeView(self, treestore = self.mailbox_tree_store)
+        self.message_view       = Gtk.TextView()
+        self.index_view         = Gtk.TextView()
 
-        self.vp = Gtk.HPaned()
-        self.vp.set_position(2000)
-        self.vp.add1(self.mailbox_tree_view.get_widget())
-        self.tv = Gtk.TextView()
-        self.vp.add2(self.tv)
-        self.vp.set_position(self.settings['gui.mainwindow.hsplit'])
-        self.add(self.vp)
+        self.vpaned = Gtk.VPaned()
+        self.vpaned.add1(self.index_view)
+        self.vpaned.add2(self.message_view)
+
+        self.hpaned = Gtk.HPaned()
+        self.hpaned.set_position(2000)
+        self.hpaned.add1(self.mailbox_tree_view.get_widget())
+        self.hpaned.add2(self.vpaned)
+        self.hpaned.set_position(self.settings['gui.mainwindow.hsplit'])
+        self.add(self.hpaned)
 
         print "hsplit="+str(self.settings['gui.mainwindow.hsplit'])
-        self.vp.set_position(2000)
+        self.hpaned.set_position(2000)
 
     def select_mailbox(self, mbox):
         print ("MainWindow: user selected mailbox: "+mbox.name)
-        self.tv.get_buffer().set_text("MainWindow: user selected mailbox: "+mbox.name)
+        self.index_view.get_buffer().set_text("MainWindow: user selected mailbox: "+mbox.name)
